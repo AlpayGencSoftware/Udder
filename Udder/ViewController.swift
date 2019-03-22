ef72727a9ba39d0ca91310e88dd0f532715d6d73
Alpay Genç <alpaygenc@alpay-imac.local>
2019-03-22 21:27:40 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Alpay Genc on 22.03.2019.
+//  Copyright © 2019 Liberty App Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
